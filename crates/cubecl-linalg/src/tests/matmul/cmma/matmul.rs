e501328df41dd01f1cf9d510e9de2452404fa7ed
louisfd <louisfd94@gmail.com>
2024-09-18 13:22:12 -0400

    wip

--- crates/cubecl-linalg/src/tests/matmul/cmma/matmul.rs
+++ crates/cubecl-linalg/src/tests/matmul/cmma/matmul.rs
@@ -8,48 +8,6 @@ macro_rules! testgen_cmma_matmul {
         }
 
         #[test]
-        pub fn test_matmul_cmma_16_16() {
-            tests::matmul_tests::test_matmul_cmma_16_16::<TestRuntime>(&Default::default())
-        }
-
-        #[test]
-        pub fn test_matmul_cmma_32_16() {
-            tests::matmul_tests::test_matmul_cmma_32_16::<TestRuntime>(&Default::default())
-        }
-
-        #[test]
-        pub fn test_matmul_cmma_32_32() {
-            tests::matmul_tests::test_matmul_cmma_32_32::<TestRuntime>(&Default::default())
-        }
-
-        #[test]
-        pub fn test_matmul_cmma_64_16() {
-            tests::matmul_tests::test_matmul_cmma_64_16::<TestRuntime>(&Default::default())
-        }
-
-        #[test]
-        pub fn test_matmul_cmma_64_32() {
-            tests::matmul_tests::test_matmul_cmma_64_32::<TestRuntime>(&Default::default())
-        }
-
-        #[test]
-        #[ignore] // Activate only for f16
-        pub fn test_matmul_cmma_64_64() {
-            tests::matmul_tests::test_matmul_cmma_64_64::<TestRuntime>(&Default::default())
-        }
-
-        #[test]
-        pub fn test_matmul_cmma_128_16() {
-            tests::matmul_tests::test_matmul_cmma_128_16::<TestRuntime>(&Default::default())
-        }
-
-        #[test]
-        #[ignore] // Activate only for f16
-        pub fn test_matmul_cmma_128_32() {
-            tests::matmul_tests::test_matmul_cmma_128_32::<TestRuntime>(&Default::default())
-        }
-
-        #[test]
         pub fn test_matmul_cmma_several_cubes() {
             tests::matmul_tests::test_matmul_cmma_several_cubes::<TestRuntime>(&Default::default())
         }
@@ -76,6 +34,326 @@ macro_rules! testgen_cmma_matmul {
         #[test]
         pub fn test_matmul_cmma_vec2_shapes() {
             tests::matmul_tests::test_matmul_cmma_vec2_shapes::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_16_16_16() {
+            tests::matmul_tests::test_matmul_cmma_16_16_16::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_16_16_32() {
+            tests::matmul_tests::test_matmul_cmma_16_16_32::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_16_16_64() {
+            tests::matmul_tests::test_matmul_cmma_16_16_64::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_16_16_128() {
+            tests::matmul_tests::test_matmul_cmma_16_16_128::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_16_32_16() {
+            tests::matmul_tests::test_matmul_cmma_16_32_16::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_16_32_32() {
+            tests::matmul_tests::test_matmul_cmma_16_32_32::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_16_32_64() {
+            tests::matmul_tests::test_matmul_cmma_16_32_64::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_16_32_128() {
+            tests::matmul_tests::test_matmul_cmma_16_32_128::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_16_64_16() {
+            tests::matmul_tests::test_matmul_cmma_16_64_16::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_16_64_32() {
+            tests::matmul_tests::test_matmul_cmma_16_64_32::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_16_64_64() {
+            tests::matmul_tests::test_matmul_cmma_16_64_64::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_16_64_128() {
+            tests::matmul_tests::test_matmul_cmma_16_64_128::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_16_128_16() {
+            tests::matmul_tests::test_matmul_cmma_16_128_16::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_16_128_32() {
+            tests::matmul_tests::test_matmul_cmma_16_128_32::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_16_128_64() {
+            tests::matmul_tests::test_matmul_cmma_16_128_64::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_16_128_128() {
+            tests::matmul_tests::test_matmul_cmma_16_128_128::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_32_16_16() {
+            tests::matmul_tests::test_matmul_cmma_32_16_16::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_32_16_32() {
+            tests::matmul_tests::test_matmul_cmma_32_16_32::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_32_16_64() {
+            tests::matmul_tests::test_matmul_cmma_32_16_64::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_32_16_128() {
+            tests::matmul_tests::test_matmul_cmma_32_16_128::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_32_32_16() {
+            tests::matmul_tests::test_matmul_cmma_32_32_16::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_32_32_32() {
+            tests::matmul_tests::test_matmul_cmma_32_32_32::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_32_32_64() {
+            tests::matmul_tests::test_matmul_cmma_32_32_64::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_32_32_128() {
+            tests::matmul_tests::test_matmul_cmma_32_32_128::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_32_64_16() {
+            tests::matmul_tests::test_matmul_cmma_32_64_16::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_32_64_32() {
+            tests::matmul_tests::test_matmul_cmma_32_64_32::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_32_64_64() {
+            tests::matmul_tests::test_matmul_cmma_32_64_64::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_32_64_128() {
+            tests::matmul_tests::test_matmul_cmma_32_64_128::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_32_128_16() {
+            tests::matmul_tests::test_matmul_cmma_32_128_16::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_32_128_32() {
+            tests::matmul_tests::test_matmul_cmma_32_128_32::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_32_128_64() {
+            tests::matmul_tests::test_matmul_cmma_32_128_64::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_32_128_128() {
+            tests::matmul_tests::test_matmul_cmma_32_128_128::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_64_16_16() {
+            tests::matmul_tests::test_matmul_cmma_64_16_16::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_64_16_32() {
+            tests::matmul_tests::test_matmul_cmma_64_16_32::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_64_16_64() {
+            tests::matmul_tests::test_matmul_cmma_64_16_64::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_64_16_128() {
+            tests::matmul_tests::test_matmul_cmma_64_16_128::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_64_32_16() {
+            tests::matmul_tests::test_matmul_cmma_64_32_16::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_64_32_32() {
+            tests::matmul_tests::test_matmul_cmma_64_32_32::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_64_32_64() {
+            tests::matmul_tests::test_matmul_cmma_64_32_64::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_64_32_128() {
+            tests::matmul_tests::test_matmul_cmma_64_32_128::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_64_64_16() {
+            tests::matmul_tests::test_matmul_cmma_64_64_16::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_64_64_32() {
+            tests::matmul_tests::test_matmul_cmma_64_64_32::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_64_64_64() {
+            tests::matmul_tests::test_matmul_cmma_64_64_64::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_64_64_128() {
+            tests::matmul_tests::test_matmul_cmma_64_64_128::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_64_128_16() {
+            tests::matmul_tests::test_matmul_cmma_64_128_16::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_64_128_32() {
+            tests::matmul_tests::test_matmul_cmma_64_128_32::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_64_128_64() {
+            tests::matmul_tests::test_matmul_cmma_64_128_64::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_64_128_128() {
+            tests::matmul_tests::test_matmul_cmma_64_128_128::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_128_16_16() {
+            tests::matmul_tests::test_matmul_cmma_128_16_16::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_128_16_32() {
+            tests::matmul_tests::test_matmul_cmma_128_16_32::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_128_16_64() {
+            tests::matmul_tests::test_matmul_cmma_128_16_64::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_128_16_128() {
+            tests::matmul_tests::test_matmul_cmma_128_16_128::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_128_32_16() {
+            tests::matmul_tests::test_matmul_cmma_128_32_16::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_128_32_32() {
+            tests::matmul_tests::test_matmul_cmma_128_32_32::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_128_32_64() {
+            tests::matmul_tests::test_matmul_cmma_128_32_64::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_128_32_128() {
+            tests::matmul_tests::test_matmul_cmma_128_32_128::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_128_64_16() {
+            tests::matmul_tests::test_matmul_cmma_128_64_16::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_128_64_32() {
+            tests::matmul_tests::test_matmul_cmma_128_64_32::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_128_64_64() {
+            tests::matmul_tests::test_matmul_cmma_128_64_64::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_128_64_128() {
+            tests::matmul_tests::test_matmul_cmma_128_64_128::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_128_128_16() {
+            tests::matmul_tests::test_matmul_cmma_128_128_16::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_128_128_32() {
+            tests::matmul_tests::test_matmul_cmma_128_128_32::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_128_128_64() {
+            tests::matmul_tests::test_matmul_cmma_128_128_64::<TestRuntime>(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_cmma_128_128_128() {
+            tests::matmul_tests::test_matmul_cmma_128_128_128::<TestRuntime>(&Default::default())
         }
     };
 }
